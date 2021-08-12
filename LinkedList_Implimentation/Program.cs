f5f248f0f66cfdaefc7e3c83a020fff07ecb3def
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-12 23:22:07 +0530

    feat:Creating a simple linked list

--- LinkedList_Implimentation/Program.cs
+++ LinkedList_Implimentation/Program.cs
@@ -7,6 +7,10 @@ namespace LinkedList_Implimentation
         static void Main(string[] args)
         {
             Console.WriteLine("Wlcome to LinkedList program");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
         }
     }
 }
