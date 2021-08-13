36d7886d1d109afd9e635ce11044285858089784
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-13 20:39:55 +0530

    feat:Sorted LinkedList

--- LinkedList_Implimentation/Program.cs
+++ LinkedList_Implimentation/Program.cs
@@ -7,27 +7,34 @@ namespace LinkedList_Implimentation
         static void Main(string[] args)
         {
             Console.WriteLine("Wlcome to LinkedList program");
-            LinkedList list = new LinkedList();
+            //LinkedList list = new LinkedList();
+            //list.Add(56);
+            //list.Add(70);
+            //Console.WriteLine("The list before inserting 30 is : ");
+            //list.Display();
+            //list.InsertAtParticularPosition(2,30);
+            //Console.WriteLine("The list after inserting 30 is : ");
+            //list.Display();
+            //list.InsertAtParticularPosition(3, 40);
+            //Console.WriteLine("The list after inserting 40 is :  ");
+            //list.Display();
+            ////list.RemoveLastNode();
+            ////Console.WriteLine("The list after removing last node is : ");
+            ////list.Display();
+            ////Console.WriteLine("Serach elment");
+            ////list.Search(30);
+            ////list.Display();
+            //Console.WriteLine("After deleting 40");
+            //list.Delete(40);
+            //list.Display();
+            SortedLinkedList list = new SortedLinkedList();
             list.Add(56);
+            list.Add(30);
+            list.Add(40);
             list.Add(70);
-            Console.WriteLine("The list before inserting 30 is : ");
+            Console.WriteLine("The ordered list after inserting 56,30,40,70 using sorted linked list is : ");
             list.Display();
-            list.InsertAtParticularPosition(2,30);
-            Console.WriteLine("The list after inserting 30 is : ");
-            list.Display();
-            list.InsertAtParticularPosition(3, 40);
-            Console.WriteLine("The list after inserting 40 is :  ");
-            list.Display();
-            //list.RemoveLastNode();
-            //Console.WriteLine("The list after removing last node is : ");
-            //list.Display();
-            //Console.WriteLine("Serach elment");
-            //list.Search(30);
-            //list.Display();
-            Console.WriteLine("After deleting 40");
-            list.Delete(40);
-            list.Display();
-            
+
 
         }
     }
